16646dd52f2cfbda1034efae87d09cb65eb95a31
boring <m2008m1033m@gmail.com>
2024-10-27 12:58:22 +1100

    refactor: moved python files into backend directory

--- backend/main.py
+++ backend/main.py
@@ -2,10 +2,16 @@ from flask import Flask
 from markupsafe import escape
 from dotenv import load_dotenv
 
+from database.db import Db
+from database.migration_manager import MigrationManager
 from ssvc_score_evaluator import SsvcScoreEvaluator
 from utils import dataclass_to_camelcase_dict
 
 load_dotenv()
+
+# Apply database migrations
+migration_manager = MigrationManager(Db(), 'database/migrations')
+migration_manager.migrate()
 
 app = Flask(__name__)
 
